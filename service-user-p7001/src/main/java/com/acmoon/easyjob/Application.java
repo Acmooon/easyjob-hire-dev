abf4f1377664247d3a3cc2796ffd633ac8dcc493
acmoon <acmoon.hzr@qq.com>
2024-02-22 15:25:18 +0800

    Spring Cloud/Boot/Alibaba版本兼容；集成Nacos

--- service-user-p7001/src/main/java/com/acmoon/easyjob/Application.java
+++ service-user-p7001/src/main/java/com/acmoon/easyjob/Application.java
@@ -3,8 +3,10 @@ package com.acmoon.easyjob;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient    // 开启服务注册与发现功能
 @MapperScan(basePackages = "com.acmoon.easyjob.mapper")
 public class Application {
     public static void main(String[] args) {
